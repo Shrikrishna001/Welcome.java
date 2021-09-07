1f5a0758034d30423bd1467829162e10f2343b5f
Shrikrishna001 <86207062+Shrikrishna001@users.noreply.github.com>
2021-09-07 20:44:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 
-public static void main(String[] args) {
-    System.out.println("Hey Whats UP");
-    } 
+	public static void main(String[] args) {
+		System.out.println("Hello World Welcome To Java Programing World");
 
+	}
 
 }
